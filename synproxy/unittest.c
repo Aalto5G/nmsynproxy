aa8fbc7cdc9037f63a5daf8e629ccac7f4a69b80
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2017-04-22 12:33:29 +0300

    Test RST for SYN proxied connections

--- synproxy/unittest.c
+++ synproxy/unittest.c
@@ -2345,6 +2345,252 @@ static void established_rst_downlink(void)
   synproxy_free(&synproxy);
 }
 
+static void syn_proxy_rst_uplink(void)
+{
+  struct port outport;
+  uint64_t time64;
+  struct packet *pktstruct;
+  struct linked_list_head head;
+  struct linkedlistfunc_userdata ud;
+  char pkt[14+20+20] = {0};
+  void *ether, *ip, *tcp;
+  char cli_mac[6] = {0x02,0,0,0,0,0x04};
+  char lan_mac[6] = {0x02,0,0,0,0,0x01};
+  uint32_t isn1 = 0x12345678;
+  uint32_t isn;
+  //uint32_t isn2 = 0x87654321;
+  struct synproxy synproxy;
+  struct ll_alloc_st st;
+  struct worker_local local;
+  struct synproxy_hash_entry *e;
+  struct conf conf = CONF_INITIALIZER;
+
+  confyydirparse(argv0, "conf.txt", &conf, 0);
+  synproxy_init(&synproxy, &conf);
+
+  if (ll_alloc_st_init(&st, POOL_SIZE, BLOCK_SIZE) != 0)
+  {
+    abort();
+  }
+
+  worker_local_init(&local, &synproxy, 1);
+
+  linked_list_head_init(&head);
+  ud.head = &head;
+  outport.userdata = &ud;
+  outport.portfunc = linkedlistfunc;
+
+  time64 = gettime64();
+
+  synproxy_handshake_impl(
+    &synproxy, &local, &st, (10<<24)|4, (11<<24)|3, 12345, 54321,
+    &isn, 1, 1, 1);
+
+  e = synproxy_hash_get(&local, (10<<24)|4, 12345, (11<<24)|3, 54321);
+  if (e == NULL)
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "state entry not found");
+    exit(1);
+  }
+
+  ether = pkt;
+  memset(pkt, 0, sizeof(pkt));
+  memcpy(ether_dst(ether), lan_mac, 6);
+  memcpy(ether_src(ether), cli_mac, 6);
+  ether_set_type(ether, ETHER_TYPE_IP);
+  ip = ether_payload(ether);
+  ip_set_version(ip, 4);
+  ip_set_hdr_len(ip, 20);
+  ip_set_total_len(ip, sizeof(pkt) - 14);
+  ip_set_dont_frag(ip, 1);
+  ip_set_id(ip, 123);
+  ip_set_ttl(ip, 64);
+  ip_set_proto(ip, 6);
+  ip_set_src(ip, (10<<24)|4);
+  ip_set_dst(ip, (11<<24)|3);
+  ip_set_hdr_cksum_calc(ip, 20);
+  tcp = ip_payload(ip);
+  tcp_set_src_port(tcp, 12345);
+  tcp_set_dst_port(tcp, 54321);
+  tcp_set_rst_on(tcp);
+  tcp_set_data_offset(tcp, 20);
+  tcp_set_seq_number(tcp, isn1 + 1);
+  tcp_set_cksum_calc(ip, 20, tcp, sizeof(pkt) - 14 - 20);
+
+  pktstruct = ll_alloc_st(&st, packet_size(sizeof(pkt)));
+  pktstruct->direction = PACKET_DIRECTION_UPLINK;
+  pktstruct->sz = sizeof(pkt);
+  memcpy(packet_data(pktstruct), pkt, sizeof(pkt));
+  if (uplink(&synproxy, &local, pktstruct, &outport, time64, &st))
+  {
+    ll_free_st(&st, pktstruct);
+  }
+
+  pktstruct = fetch_packet(&head);
+  if (pktstruct == NULL)
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "no packet out");
+    exit(1);
+  }
+  if (pktstruct->sz != sizeof(pkt))
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "output packet size doesn't agree");
+    exit(1);
+  }
+  if (pktstruct->direction != PACKET_DIRECTION_UPLINK)
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "output packet direction doesn't agree");
+    exit(1);
+  }
+  tcp_set_seq_number(tcp, isn + 1);
+  tcp_set_cksum_calc(ip, 20, tcp, sizeof(pkt) - 14 - 20);
+  if (memcmp(packet_data(pktstruct), pkt, sizeof(pkt)) != 0)
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "output packet data doesn't agree");
+    exit(1);
+  }
+  ll_free_st(&st, pktstruct);
+  pktstruct = fetch_packet(&head);
+  if (pktstruct != NULL)
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "extra packet out");
+    exit(1);
+  }
+  e = synproxy_hash_get(&local, (10<<24)|4, 12345, (11<<24)|3, 54321);
+  if (e != NULL)
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "state entry found");
+    exit(1);
+  }
+
+  ll_alloc_st_free(&st);
+  worker_local_free(&local);
+  conf_free(&conf);
+  synproxy_free(&synproxy);
+}
+
+static void syn_proxy_rst_downlink(void)
+{
+  struct port outport;
+  uint64_t time64;
+  struct packet *pktstruct;
+  struct linked_list_head head;
+  struct linkedlistfunc_userdata ud;
+  char pkt[14+20+20] = {0};
+  void *ether, *ip, *tcp;
+  char cli_mac[6] = {0x02,0,0,0,0,0x04};
+  char lan_mac[6] = {0x02,0,0,0,0,0x01};
+  //uint32_t isn1 = 0x12345678;
+  uint32_t isn2 = 0x87654321;
+  uint32_t isn;
+  struct synproxy synproxy;
+  struct ll_alloc_st st;
+  struct worker_local local;
+  struct synproxy_hash_entry *e;
+  struct conf conf = CONF_INITIALIZER;
+
+  confyydirparse(argv0, "conf.txt", &conf, 0);
+  synproxy_init(&synproxy, &conf);
+
+  if (ll_alloc_st_init(&st, POOL_SIZE, BLOCK_SIZE) != 0)
+  {
+    abort();
+  }
+
+  worker_local_init(&local, &synproxy, 1);
+
+  linked_list_head_init(&head);
+  ud.head = &head;
+  outport.userdata = &ud;
+  outport.portfunc = linkedlistfunc;
+
+  time64 = gettime64();
+
+  synproxy_handshake_impl(
+    &synproxy, &local, &st, (10<<24)|6, (11<<24)|5, 12345, 54321,
+    &isn, 1, 1, 1);
+
+  e = synproxy_hash_get(&local, (10<<24)|6, 12345, (11<<24)|5, 54321);
+  if (e == NULL)
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "state entry not found");
+    exit(1);
+  }
+
+  ether = pkt;
+  memset(pkt, 0, sizeof(pkt));
+  memcpy(ether_dst(ether), cli_mac, 6);
+  memcpy(ether_src(ether), lan_mac, 6);
+  ether_set_type(ether, ETHER_TYPE_IP);
+  ip = ether_payload(ether);
+  ip_set_version(ip, 4);
+  ip_set_hdr_len(ip, 20);
+  ip_set_total_len(ip, sizeof(pkt) - 14);
+  ip_set_dont_frag(ip, 1);
+  ip_set_id(ip, 123);
+  ip_set_ttl(ip, 64);
+  ip_set_proto(ip, 6);
+  ip_set_src(ip, (11<<24)|5);
+  ip_set_dst(ip, (10<<24)|6);
+  ip_set_hdr_cksum_calc(ip, 20);
+  tcp = ip_payload(ip);
+  tcp_set_src_port(tcp, 54321);
+  tcp_set_dst_port(tcp, 12345);
+  tcp_set_rst_on(tcp);
+  tcp_set_data_offset(tcp, 20);
+  tcp_set_seq_number(tcp, isn2 + 1);
+  tcp_set_cksum_calc(ip, 20, tcp, sizeof(pkt) - 14 - 20);
+
+  pktstruct = ll_alloc_st(&st, packet_size(sizeof(pkt)));
+  pktstruct->direction = PACKET_DIRECTION_DOWNLINK;
+  pktstruct->sz = sizeof(pkt);
+  memcpy(packet_data(pktstruct), pkt, sizeof(pkt));
+  if (downlink(&synproxy, &local, pktstruct, &outport, time64, &st))
+  {
+    ll_free_st(&st, pktstruct);
+  }
+
+  pktstruct = fetch_packet(&head);
+  if (pktstruct == NULL)
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "no packet out");
+    exit(1);
+  }
+  if (pktstruct->sz != sizeof(pkt))
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "output packet size doesn't agree");
+    exit(1);
+  }
+  if (pktstruct->direction != PACKET_DIRECTION_DOWNLINK)
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "output packet direction doesn't agree");
+    exit(1);
+  }
+  if (memcmp(packet_data(pktstruct), pkt, sizeof(pkt)) != 0)
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "output packet data doesn't agree");
+    exit(1);
+  }
+  ll_free_st(&st, pktstruct);
+  pktstruct = fetch_packet(&head);
+  if (pktstruct != NULL)
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "extra packet out");
+    exit(1);
+  }
+  e = synproxy_hash_get(&local, (10<<24)|6, 12345, (11<<24)|5, 54321);
+  if (e != NULL)
+  {
+    log_log(LOG_LEVEL_ERR, "UNIT", "state entry found");
+    exit(1);
+  }
+
+  ll_alloc_st_free(&st);
+  worker_local_free(&local);
+  conf_free(&conf);
+  synproxy_free(&synproxy);
+}
+
 static void three_way_handshake_ulretransmit(void)
 {
   struct synproxy synproxy;
@@ -2793,6 +3039,10 @@ int main(int argc, char **argv)
   syn_proxy_downlink();
 
   syn_proxy_uplink_downlink();
+
+  syn_proxy_rst_uplink();
+
+  syn_proxy_rst_downlink();
 
   return 0;
 }
